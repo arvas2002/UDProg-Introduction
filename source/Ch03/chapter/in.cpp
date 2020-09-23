9c59dc8982ac54f87dbbf0779febd66d9e7e92fe
arvas2002 <arvas2002@gmail.com>
2020-09-23 15:28:54 +0200

    Hello world uppload

--- source/Ch03/chapter/in.cpp
+++ source/Ch03/chapter/in.cpp
@@ -2,13 +2,31 @@
 
 int main ()
 {
+	cout <<"Give me your sex(m/f) ";
+	char sex;
+	cin>>sex;
+	if (sex=='m')
+		cout<<"You'r sex is male\n";
+	else if (sex=='f')
+		cout<< "You'r sex is female\n";
+	else cout << "Wrong input\n";
 	string first_name, last_name;
+	cout<<"Give me your first name  and last name and your age\n";
 	cin>>first_name>>last_name;
 
 	int age;
 	cin>>age;
 
-	cout<<"The user name is: "<<first_name<< ' '<< last_name<< " and her/his age is: "<<age<<endl;
+
+	if (sex=='f')
+	cout<<"The user name is: "<<first_name<< ' '<< last_name<< " and her age is: "<<age<<endl;
+	if (sex=='m')
+	cout<<"The user name is: "<<first_name<< ' '<< last_name<< " and his age is: "<<age<<endl;
+
+	if (age<18||age<110) error("nem jó év");
+
+
+	
 	return 0;
 
 
